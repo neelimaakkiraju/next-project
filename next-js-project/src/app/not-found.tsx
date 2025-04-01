f794856c37b807e5ad87ed2ba6db1cc56588bd8c
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-04-01 15:45:39 +0530

    completed nested dynamic routing

---
+++ next-js-project/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const notFound = () => {
+  return (
+    <div>
+        <h1>Page Not Found</h1>
+        <h2>could not find requested resources</h2>
+    </div>
+  )
+}
+
+export default notFound
